8e83fe276fa704274062d0a3fd92340d61db919f
Gabriel Alencar <gabrielalencar33564@gmail.com>
2023-02-14 18:30:26 -0300

    fix: there must be at least one product

--- src/serializers/store.serializer.ts
+++ src/serializers/store.serializer.ts
@@ -3,5 +3,8 @@ import * as yup from "yup";
 export const createProductSerializer = yup.object().shape({
   name: yup.string().required("Name is required"),
   category: yup.string().required("Category is required"),
-  quantity: yup.number().required("Quantity is required"),
+  quantity: yup
+    .number()
+    .min(1, "there must be at least one product")
+    .required("Quantity is required"),
 });
